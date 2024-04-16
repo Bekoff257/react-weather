e9fbd45c892b6c342c6fac46fd671720759463d5
Bekof257 <behzodbekpolatov5@gmail.com>
2024-04-16 22:12:50 +0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import Loader from "./components/loader";
+import { useEffect, useState } from "react";
+import Weather from "./components/weather";
+import * as Location from "expo-location";
+import axios from "axios";
+
+const API_KEY = "22d4b636da71a82eacdc2d29860a3761";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [isLoader, setIsLoader] = useState(true);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [location, setLocation] = useState(null);
+
+  const getWeather = async (longitude, latitude) => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    setLocation(data);
+    setIsLoader(false);
+  };
+
+  const setWeather = async (query) => {
+    try {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}&units=metric`
+      );
+      setLocation(data);
+      setIsLoader(false);
+    } catch (error) {
+      setIsLoader(false);
+      Alert.alert("I can't find this city! :(. Try again!");
+    }
+  };
+
+  const getLocation = async () => {
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert("Permission to access location was denied");
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({});
+
+      getWeather(longitude, latitude);
+    } catch (error) {
+      Alert.alert("I can't find your current Location! :(. Try again!");
+    }
+  };
+
+  // getLocation();
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+  return isLoader ? (
+    <Loader />
+  ) : (
+    <Weather
+      setWeather={setWeather}
+      temp={location.main.temp}
+      name={location.name}
+      condition={location.weather[0].main}
+    />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
